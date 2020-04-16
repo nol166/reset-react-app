56405b75adf3c97e131462177ec6d892b710658d
John McCambridge <jmccamb@gmail.com>
2020-04-16 15:21:00 -0500

    refactor for diffs in package.json, add colors

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const path = process.argv[2]
 const newFiles = process.argv[3]
 let hasDir = false
 let dirtyPkg = false
-const freshPkg = fs.readFileSync('./resources/package.json')
-const currentPkg = fs.readFileSync(`${path}/package.json`)
+const freshPkgPath = './resources/package.json'
+const currentPkgPath = `${path}/package.json`
+const freshPkg = fs.readFileSync(freshPkgPath)
+const currentPkg = fs.readFileSync(currentPkgPath)
+let results = []
 
 const bold = str => {
     return '\033[1m' + str + '\033[0m'
@@ -12,6 +15,10 @@ const bold = str => {
 
 const green = str => {
     return '\x1b[36m' + str + '\x1b[0m'
+}
+
+const red = str => {
+    return '\x1b[31m' + str + `\x1b[0m`
 }
 
 // check if dir exists
@@ -23,7 +30,7 @@ if (!path) {
 
 // check for dirty package.json
 const isDirty = (current, fresh) => {
-    if (Buffer.from(current) === Buffer.from(fresh)) {
+    if (current.length === fresh.length) {
         return false
     } else {
         return true
@@ -38,23 +45,33 @@ fs.readdir(path, (err, items) => {
     console.log(bold('Current items:'))
     items.forEach(item => {
         console.log(item)
+        // check if react app has a src directory
         hasDir = item === 'src' ? true : false
         if (item === 'package.json') {
             dirtyPkg = isDirty(currentPkg, freshPkg)
             try {
                 if (dirtyPkg) {
                     //file exists, delete it
-                    console.log('deleting dirty package.json')
-                    // fs.unlinkSync(currentPkg)
+                    results.push(red('deleting dirty package.json'))
+                    fs.unlink(currentPkgPath, () => console.log('\n'))
                     // copy the clean one
-                    // fs.createReadStream(freshPkg).pipe(
-                    //     fs.createWriteStream(currentPkg)
-                    // )
-                    console.log(green('Replaced package.json'))
+                    fs.copyFile(freshPkgPath, currentPkgPath, err => {
+                        if (err) throw err
+                        results.push(bold('deleting dirty package.json'))
+                    })
+                    results.push(green('replaced package.json'))
                 }
             } catch (err) {
                 console.error(err)
             }
         }
     })
+    // log out any results at the end
+    if (results.length > 0) {
+        console.log('\n')
+        results.forEach(result => console.log(result))
+    } else {
+        console.log('')
+        console.log(bold('No reset needed'))
+    }
 })
