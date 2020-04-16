d46be6ed1b68f01162195f703268ee1ce7c60345
John McCambridge <jmccamb@gmail.com>
2020-04-16 14:31:59 -0500

    add checking for dirty package.json

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const fs = require('fs')
 const path = process.argv[2]
+const newFiles = process.argv[3]
 let hasDir = false
-let hasPackageJSON = false
+let dirtyPkg = false
+const freshPkg = fs.readFileSync('./resources/package.json')
+const currentPkg = fs.readFileSync(`${path}/package.json`)
 
 const bold = str => {
     return '\033[1m' + str + '\033[0m'
 }
 
+const green = str => {
+    return '\x1b[36m' + str + '\x1b[0m'
+}
+
 // check if dir exists
 if (!path) {
-    console.log('Usage: ' + __filename + ' path/to/directory')
+    console.log(`${bold('reset-react-app')}`)
+    console.log(`Usage: ${__filename} path/to/react_app  path/to/src_files`)
     process.exit(-1)
+}
+
+// check for dirty package.json
+const isDirty = (current, fresh) => {
+    if (Buffer.from(current) === Buffer.from(fresh)) {
+        return false
+    } else {
+        return true
+    }
 }
 
 fs.readdir(path, (err, items) => {
@@ -22,5 +39,22 @@ fs.readdir(path, (err, items) => {
     items.forEach(item => {
         console.log(item)
         hasDir = item === 'src' ? true : false
+        if (item === 'package.json') {
+            dirtyPkg = isDirty(currentPkg, freshPkg)
+            try {
+                if (dirtyPkg) {
+                    //file exists, delete it
+                    console.log('deleting dirty package.json')
+                    // fs.unlinkSync(currentPkg)
+                    // copy the clean one
+                    // fs.createReadStream(freshPkg).pipe(
+                    //     fs.createWriteStream(currentPkg)
+                    // )
+                    console.log(green('Replaced package.json'))
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }
     })
 })
